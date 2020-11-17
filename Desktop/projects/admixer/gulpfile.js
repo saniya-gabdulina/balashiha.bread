6a90537ef2271595ef9612126ad9c989275a0177
saniya.gabdulina <saniya.gabdulina@gmail.com>
2020-11-17 15:25:48 +0600

    add gulpfile

---
+++ Desktop/projects/admixer/gulpfile.js
@@ -0,0 +1,30 @@
+const{src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+
+
+// Static server
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./scss/**/*.scss", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+
+};
+
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./scss/*.scss")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
